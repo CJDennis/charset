988889e4ad3a19ba564de559804da2e8c2a9d9da
Daniel Klein <danielklein@airpost.net>
2019-11-25 20:52:59 +1100

    Pass the test

--- src/CJDennis/Charset/Charset.php
+++ src/CJDennis/Charset/Charset.php
@@ -2,12 +2,12 @@
 namespace CJDennis\Charset;
 
 class Charset {
-  public static function convert($string, $to_charset = 'ASCII') {
-    return preg_replace_callback('/\X/u', function ($match) use ($to_charset) {
+  public static function convert($string, $to_charset = 'ASCII', $replacement = '?') {
+    return preg_replace_callback('/\X/u', function ($match) use ($to_charset, $replacement) {
       $glyph = iconv('UTF-8', "{$to_charset}//IGNORE//TRANSLIT", $match[0]);
       // The documentation says that iconv() returns false on failure but it returns either '' or false depending on the library
       if ($glyph === false || $glyph === '') {
-        $glyph = '?';
+        $glyph = $replacement;
       }
       return $glyph;
     }, $string);

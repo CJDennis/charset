902c8398574adbf47212a9048ffb65654d6d1c14
Daniel Klein <danielklein@airpost.net>
2019-12-02 21:04:54 +1100

    Fix tag codepoints

--- src/CJDennis/Charset/GnuLibiconv.php
+++ src/CJDennis/Charset/GnuLibiconv.php
@@ -5,7 +5,7 @@ use Normalizer;
 
 class GnuLibiconv implements Iconv {
   public static function convert($string, $to_charset, $replacement) {
-    return preg_replace_callback('/\X/u', function ($match) use ($to_charset, $replacement) {
+    return preg_replace_callback('/\X[\x{E0020}-\x{E007F}]*/u', function ($match) use ($to_charset, $replacement) {
       $glyph = iconv('UTF-8', "{$to_charset}//IGNORE//TRANSLIT", $match[0]);
       if ($glyph === '') {
         $glyph = Normalizer::normalize($match[0], Normalizer::FORM_D);
